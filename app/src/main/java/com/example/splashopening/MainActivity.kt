37a2c325df663f866a259a2de7338fda1191c03f
STORM-PC\Takum <takumi87jn@gmail.com>
2024-01-20 18:39:01 +0900

    install splash screen

--- app/src/main/java/com/example/splashopening/MainActivity.kt
+++ app/src/main/java/com/example/splashopening/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.splashopening
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
     }
 }
