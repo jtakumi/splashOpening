58ce68909342b86a559edae4d6197bcd678d076c
STORM-PC\Takum <takumi87jn@gmail.com>
2024-01-28 16:08:52 +0900

    thread.sleep削除

--- app/src/main/java/com/example/splashopening/MainActivity.kt
+++ app/src/main/java/com/example/splashopening/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.example.splashopening
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.splashopening.screen.portalFragment
 
 class MainActivity : AppCompatActivity() {
+    private val portalFragment = portalFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(3000)
         installSplashScreen()
         setContentView(R.layout.activity_main)
-    }
+        }
+
 
     override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
         return super.onCreateView(name, context, attrs)
